574cc9ee9cbe7a2ad584b01a1899ba9beb6413b7
Jash Gusani <jashgusani20@gmail.com>
2025-06-20 12:05:49 +0530

    {style}: Improved the appearance of animations

--- src/Components/LandingMainNavbar.tsx
+++ src/Components/LandingMainNavbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LandingMainNavbar = () => {
     return (
         <>
-            <div className="flex sticky top-0 bg-[#0000008e] shadow-2xl rounded-2xl justify-between items-center h-20 px-10">
+            <div className="flex sticky top-0 bg-[#00000063] shadow-2xl rounded-2xl justify-between items-center h-20 px-10">
                 <div className="flex items-center gap-2">
                     <img src="/Logo.png" alt="Snippetix Logo" className="w-10 h-10" />
                     <h1 className="text-2xl font-bold underline">Snippetix</h1>
