4fcb62cf38c796f67413be3b48e89806db041415
Jash Gusani <jashgusani20@gmail.com>
2025-06-20 12:08:16 +0530

    {style}: Improve Style for Logo Name

--- src/Components/LandingMainNavbar.tsx
+++ src/Components/LandingMainNavbar.tsx
@@ -6,7 +6,7 @@ const LandingMainNavbar = () => {
             <div className="flex sticky top-0 bg-[#00000063] shadow-2xl rounded-2xl justify-between items-center h-20 px-10">
                 <div className="flex items-center gap-2">
                     <img src="/Logo.png" alt="Snippetix Logo" className="w-10 h-10" />
-                    <h1 className="text-2xl font-bold underline">Snippetix</h1>
+                    <h1 className="text-2xl font-bold">Snippetix</h1>
                 </div>
                 <div>
                     <button className="bg-zinc-800 cursor-pointer hover:scale-105 transition text-white px-4 py-2 rounded">
