d0bbecadabce8bd9ae28520e5c413e35ea3815d1
Jash Gusani <jashgusani78@gmail.com>
2025-08-13 17:39:17 +0530

    text: checking

--- src/Components/Landing/LandingMainNavbar.tsx
+++ src/Components/Landing/LandingMainNavbar.tsx
@@ -85,7 +85,7 @@ const LandingMainNavbar = ({ isDashboard, setStep , isProblemDetails }: { isDash
                 {!isDashboard && !isProblemDetails && (
                     <button
                         className="bg-zinc-800 cursor-pointer hover:scale-105 transition text-white px-4 py-2 rounded"
-                        onClick={() => route.push('user/dashboard')}
+                        onClick={() => route.replace('user/dashboard')}
                     >
                         Get Started
                     </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import LandingPage from "@/Pages/LandingPage";
-
+// import Page from './user/dashboard/page'
 export default function Home() {
   return (
     <>
+    {/* <Page /> */}
     <LandingPage />
     </>
   );
